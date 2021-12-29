8d630066fd05cd80d0933ae662f16d1d60351cf1
Michael Birken <michael.birken@factset.com>
2021-12-29 17:32:20 -0500

    creating

--- src/main/java/tetriscircuits/Main.java
+++ src/main/java/tetriscircuits/Main.java
@@ -7,12 +7,18 @@ import tetriscircuits.parser.Parser;
 public class Main {
 
     public void launch() throws Exception {
-        final Map<String, Component> components = new HashMap<>();
-        final Parser parser = new Parser();
-        parser.parse(components, "circuits/components.txt");
-        for (final Component component : components.values()) {
-            System.out.println(component);
+//        final Map<String, Component> components = new HashMap<>();
+//        final Parser parser = new Parser();
+//        parser.parse(components, "circuits/components.txt");
+//        for (final Component component : components.values()) {
+//            System.out.println(component);
+//        }
+
+        final Playfield playfield = new Playfield(26, 10, 2);
+        for(int i = 0; i < 26; ++i) {
+            playfield.set(i, 4, i + 1);
         }
+        System.out.println(playfield);
     }
     
     public static void main(final String... args) throws Exception {
--- src/main/java/tetriscircuits/Playfield.java
+++ src/main/java/tetriscircuits/Playfield.java
@@ -2,14 +2,15 @@ package tetriscircuits;
 
 public class Playfield {
     
-                                                // 2^:  0   1   2   3   4   5
+                                          // 2^:  0   1   2   3   4   5
     private static final int[] POWERS_OF_TWO = {  1,  2,  4,  8, 16, 32 };
-    private static final int[] MASKS = { 0b000001, 0b000011, 0b000111, 0b001111, 0b011111, 0b111111 };
+    private static final int[] MASKS = { 0x0000_0001, 0x0000_0003, 0x0000_000F, 0x0000_00FF, 0x0000_FFFF, 0xFFFF_FFFF };
     
     private final int width;
     private final int height;
     private final int power;
     private final int mask;
+    private final int mask2;
     private final int shift;
     
     private final int[][] data;
@@ -24,16 +25,16 @@ public class Playfield {
                     break outer;
                 }
             }
-            throw new IllegalArgumentException("bisPerCell must be a power of 2.");
+            throw new IllegalArgumentException("bitsPerCell must be a power of 2.");
         }
         
         this.width = width;
         this.height = height;
-        
+                
         shift = 5 - power;
-        
+        mask2 = POWERS_OF_TWO[shift] - 1;
         int w = width >> shift;
-        if ((w << shift) < width) {
+        if ((width & mask2) != 0) {
             ++w;
         }
         data = new int[height][w];
@@ -46,6 +47,40 @@ public class Playfield {
         if (y < 0) {
             return 0;
         }
-        return (data[y][x >> shift] >> ((x & 7) << power)) & 0x0F;
+        return (data[y][x >> shift] >> ((x & mask2) << power)) & mask;
     }
+    
+    public void set(final int x, final int y, final int color) {
+        if (x < 0 || y < 0 || x >= width || y >= height) {
+            return;
+        }
+        final int[] row = data[y];
+        final int s = (x & mask2) << power;
+        row[x >> shift] = (row[x >> shift] & ~(mask << s)) | ((color & mask) << s);        
+    }
+    
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder();
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                final int color = get(x, y);
+                if (color == 0) {
+                    sb.append('.');
+                } else {
+                    sb.append((char)('A' + color - 1));
+                }
+            }
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
+    }    
 }
