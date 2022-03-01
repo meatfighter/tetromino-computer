5141c54888faec655bc69c4afce5ac408fd85ba7
Michael Birken <michael.birken@factset.com>
2022-03-01 17:30:44 -0500

    playfield

---
+++ src/main/java/tetriscircuits/computer/ui/PlayfieldPanel.java
@@ -0,0 +1,25 @@
+package tetriscircuits.computer.ui;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class PlayfieldPanel extends JPanel {
+    
+    private static final Color[] COLORS = {
+        new Color(0xB802FD),
+        new Color(0x1801FF),
+        new Color(0xFE103C),
+        new Color(0xFFDE00),
+        new Color(0x66FD00),
+        new Color(0xFF7308),
+        new Color(0x00E6FE),
+    };
+    
+    
+
+    @Override
+    protected void paintComponent(final Graphics g) {
+        
+    }    
+}
