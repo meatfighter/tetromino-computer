ab8a698527c9c2cff10b91e80d96bb250aa558a3
Michael Birken <michael.birken@factset.com>
2022-03-03 18:28:15 -0500

    playfield

---
+++ src/main/java/tetriscircuits/computer/ui/Main.java
@@ -0,0 +1,29 @@
+package tetriscircuits.computer.ui;
+
+import com.bulenkov.darcula.DarculaLaf;
+import java.awt.EventQueue;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import tetriscircuits.ui.UiUtil;
+
+public class Main {
+    
+    private void launch() {
+        EventQueue.invokeLater(this::createFrame);
+    }
+    
+    private void createFrame() {
+        try {
+            UIManager.setLookAndFeel(new DarculaLaf()); 
+        } catch (final UnsupportedLookAndFeelException e) {
+        }
+        final PlayfieldFrame frame = new PlayfieldFrame();        
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+    
+    public static void main(final String... args) {
+        new Main().launch();
+    }    
+}
