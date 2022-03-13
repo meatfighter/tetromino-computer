ecc43c26729f11cebe788f16e39f5d72909a8ba3
Michael Birken <o__1@hotmail.com>
2022-03-13 11:46:12 -0400

    mappings

--- src/main/java/tetriscircuits/computer/mapping/MapMaker.java
+++ src/main/java/tetriscircuits/computer/mapping/MapMaker.java
@@ -1,5 +1,42 @@
 package tetriscircuits.computer.mapping;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
 public class MapMaker {
     
+    public static final String WORKSPACE_DIR = "workspace";
+    
+    public void launch() throws Exception {
+        final Map<String, File> tetrisScriptFiles = findTetrisScriptFiles();
+        for (Map.Entry<String, File> entry : tetrisScriptFiles.entrySet()) {
+            System.out.format("%s -> %s%n", entry.getKey(), entry.getValue());
+        }
+    }
+    
+    private Map<String, File> findTetrisScriptFiles() {
+        final Map<String, File> files = new HashMap<>();
+        findTetrisScriptFiles(new File(WORKSPACE_DIR), files);
+        return files;
+    }
+    
+    private void findTetrisScriptFiles(final File directory, final Map<String, File> files) {
+        for (final File file : directory.listFiles()) {
+            if (file.isDirectory()) {
+                findTetrisScriptFiles(file, files);
+                continue;
+            }
+            if (!file.getName().endsWith(".t")) {
+                continue;
+            }
+            final String filename = file.getName();
+            final String componentName = filename.substring(0, filename.indexOf('.'));
+            files.put(componentName, file);
+        }
+    }    
+    
+    public static void main(final String... args) throws Exception {
+        new MapMaker().launch();
+    }
 }
--- src/main/java/tetriscircuits/computer/mapping/Mapping.java
+++ src/main/java/tetriscircuits/computer/mapping/Mapping.java
@@ -18,192 +18,79 @@ public class Mapping {
             throw new IOException("Invalid map type.");
         }
         
-        switch(mapType) {
-            case ONE_BYTE:
-                return readOneByte(in);
-            case TWO_BYTES:
-                return readTwoBytes(in);
-            case BIT_TWO_BYTES:
-                return readBitTwoBytes(in);
-            default:
-                return readTwoBytesBit(in);                
+        switch (mapTypeValue) {
+            
         }
     }
-    
-    private static Mapping readOneByte(final InputStream in) throws IOException {
-        
-        final int[] map = new int[256];
-        for (int i = 0; i < 256; ++i) {
-            final int b = in.read();
-            if (b < 0) {
-                throw new IOException("Unexpected end of file.");
-            }
-            map[i] = b;
-        }
-        return new Mapping(map);
-    }
-    
-    private static Mapping readTwoBytes(final InputStream in) throws IOException {
-        
-        final int[][][] map = new int[256][256][2];
-        for (int i = 0; i < 256; ++i) {
-            for (int j = 0; j < 256; ++j) {
-                final int a = in.read();
-                final int b = in.read();
-                if (a < 0 || b < 0) {
-                    throw new IOException("Unexpected end of file.");
-                }
-                map[i][j][0] = a;
-                map[i][j][1] = b;
-            }
-        }
-        return new Mapping(map);
-    }
-
-    private static Mapping readBitTwoBytes(final InputStream in) throws IOException {
-        
-        final int[][][][] map = new int[2][256][256][3];
-        for (int i = 0; i < 2; ++i) {
-            for (int j = 0; j < 256; ++j) {
-                for (int k = 0; k < 256; ++k) {
-                    final int a = in.read();
-                    final int b = in.read();
-                    final int c = in.read();
-                    if (a < 0 || b < 0 || c < 0) {
-                        throw new IOException("Unexpected end of file.");
-                    }
-                    map[i][j][k][0] = a;
-                    map[i][j][k][1] = b;
-                    map[i][j][k][2] = c;
-                }
-            }
-        }
-        return new Mapping(map);
-    }
-
-    private static Mapping readTwoBytesBit(final InputStream in) throws IOException {
-        
-        final int[][][][] map = new int[256][256][2][3];
-        for (int i = 0; i < 256; ++i) {
-            for (int j = 0; j < 256; ++j) {
-                for (int k = 0; k < 2; ++k) {
-                    final int a = in.read();
-                    final int b = in.read();
-                    final int c = in.read();
-                    if (a < 0 || b < 0 || c < 0) {
-                        throw new IOException("Unexpected end of file.");
-                    }
-                    map[i][j][k][0] = a;
-                    map[i][j][k][1] = b;
-                    map[i][j][k][2] = c;
-                }
-            }
-        }
-        return new Mapping(map);
-    }  
     
     private final MapType mapType;
-    private final int[] one;
-    private final int[][][] two;
-    private final int[][][][] three;
+    private final int[] map;
 
+    public Mapping(final MapType mapType, final int[] map) {
+        this.mapType = mapType;
+        this.map = map;
+    }
+    
     public Mapping(final int[] map) {
-        mapType = MapType.ONE_BYTE;
-        one = map;
-        two = null;
-        three = null;
+        this(MapType.ONE_BYTE, map);
     }
 
     public Mapping(final int[][][] map) {
         mapType = MapType.TWO_BYTES;
-        one = null;
-        two = map;
-        three = null;
+        this.map = new int[256 * 256 * 2];
+        int index = 0;
+        for (int i = 0; i < 256; ++i) {
+            final int[][] p = map[(map.length == 2) ? (i & 1) : i];
+            for (int j = 0; j < 256; ++j) {
+                final int[] q = p[(p.length == 2) ? (j & 1) : j];
+                for (int k = 0; k < 2; ++k) {
+                    this.map[index++] = q[k];
+                }
+            }
+        }
     }
 
     public Mapping(final int[][][][] map) {
-        mapType = (map.length == 2) ? MapType.BIT_TWO_BYTES : MapType.TWO_BYTES_BIT;
-        one = null;
-        two = null;
-        three = map;
+        this.map = new int[256 * 256 * 2 * 3];
+        if (map.length == 2) {
+            mapType = MapType.BIT_TWO_BYTES;
+            int index = 0;
+            for (int i = 0; i < 2; ++i) {
+                for (int j = 0; j < 256; ++j) {
+                    for (int k = 0; k < 256; ++k) {
+                        for (int l = 0; l < 3; ++l) {
+                            this.map[index++] = map[i][j][k][l];
+                        }
+                    }
+                }
+            }
+        } else {
+            mapType = MapType.TWO_BYTES_BIT;
+            int index = 0;
+            for (int i = 0; i < 256; ++i) {
+                for (int j = 0; j < 256; ++j) {
+                    for (int k = 0; k < 2; ++k) {
+                        for (int l = 0; l < 3; ++l) {
+                            this.map[index++] = map[i][j][k][l];
+                        }
+                    }
+                }
+            }
+        }        
     }
 
     public MapType getMapType() {
         return mapType;
     }
 
-    public int[] getOne() {
-        return one;
+    public int[] getMap() {
+        return map;
     }
-
-    public int[][][] getTwo() {
-        return two;
-    }
-
-    public int[][][][] getThree() {
-        return three;
-    }  
     
     public void write(final OutputStream out) throws IOException {
-        switch (mapType) {
-            case ONE_BYTE:
-                writeOneByte(out);
-                break;
-            case TWO_BYTES:
-                writeTwoBytes(out);
-                break;
-            case BIT_TWO_BYTES:
-                writeBitTwoBytes(out);
-                break;
-            case TWO_BYTES_BIT:
-                writeTwoBytesBit(out);
-                break;
-        }
-    }
-    
-    private void writeOneByte(final OutputStream out) throws IOException {
         out.write(mapType.getValue());
-        for (int i = 0; i < 256; ++i) {
-            out.write(one[i]);
-        }
-    }
-    
-    private void writeTwoBytes(final OutputStream out) throws IOException {
-        out.write(mapType.getValue());
-        for (int i = 0; i < 256; ++i) {
-            final int[][] p = two[(two.length == 2) ? (i & 1) : i];
-            for (int j = 0; j < 256; ++j) {
-                final int[] q = p[(p.length == 2) ? (j & 1) : j];
-                for (int k = 0; k < 2; ++k) {
-                    out.write(q[k]);
-                }
-            }
-        }
-    }
-    
-    private void writeBitTwoBytes(final OutputStream out) throws IOException {
-        out.write(mapType.getValue());
-        for (int i = 0; i < 2; ++i) {
-            for (int j = 0; j < 256; ++j) {
-                for (int k = 0; k < 256; ++k) {
-                    for (int l = 0; l < 3; ++l) {
-                        out.write(three[i][j][k][l]);
-                    }
-                }
-            }
-        }
-    }
-    
-    private void writeTwoBytesBit(final OutputStream out) throws IOException {
-        out.write(mapType.getValue());
-        for (int i = 0; i < 256; ++i) {
-            for (int j = 0; j < 256; ++j) {
-                for (int k = 0; k < 2; ++k) {
-                    for (int l = 0; l < 3; ++l) {
-                        out.write(three[i][j][k][l]);
-                    }
-                }
-            }
+        for (int i = 0; i < map.length; ++i) {
+            out.write(map[i]);
         }
     }
 }
