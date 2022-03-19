c071f4529de749d46155814886e9d82acc3ef3be
Michael Birken <o__1@hotmail.com>
2022-03-19 09:16:02 -0400

    MINUS_C

--- src/main/java/tetriscircuits/assembler/GenerateInstructionDetectors.java
+++ src/main/java/tetriscircuits/assembler/GenerateInstructionDetectors.java
@@ -34,6 +34,7 @@ public class GenerateInstructionDetectors {
             }
             saveFiles(name, operator.getOpcode());
         }
+        saveFiles("MINUS", 0b1000_0000);
     }
     
     private void saveFiles(final String name, final int opcode) throws Exception {
