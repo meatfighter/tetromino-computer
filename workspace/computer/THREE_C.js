513f6b39679f04e6e3921ca57afc898ae4041477
Michael Birken <o__1@hotmail.com>
2022-03-26 14:16:33 -0400

    three

---
+++ workspace/computer/THREE_C.js
@@ -0,0 +1,17 @@
+io7 = ii7;
+io6 = ii6;
+io5 = ii5;
+io4 = ii4;
+io3 = ii3;
+io2 = ii2;
+io1 = ii1;
+io0 = ii0;
+
+co7 = false;
+co6 = false;
+co5 = false;
+co4 = false;
+co3 = false;
+co2 = false;
+co1 = false;
+co0 = !ii7 && !ii6 && ii5 && !ii4;
