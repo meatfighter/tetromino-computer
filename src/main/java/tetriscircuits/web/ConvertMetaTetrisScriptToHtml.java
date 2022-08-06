0f074b227105d3ed67dc11448772f6d9d86184df
Michael Birken <o__1@hotmail.com>
2022-08-06 11:53:30 -0400

    microcode

--- src/main/java/tetriscircuits/web/ConvertMetaTetrisScriptToHtml.java
+++ src/main/java/tetriscircuits/web/ConvertMetaTetrisScriptToHtml.java
@@ -1,23 +1,25 @@
 package tetriscircuits.web;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.PrintStream;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
-public class ConvertMicrocodeToHtml {
+public class ConvertMetaTetrisScriptToHtml {
     
     private static final Pattern CODE_PATTERN = Pattern.compile("([^\\s]+\\s+)(\\d+\\s*)(;.*)?");
 
-    public void launch() throws Exception {
+    public void launch(final File file, final boolean ignoreSwaps) throws Exception {
         final PrintStream out = System.out;
         printHeader(out);
-        try (final BufferedReader br = new BufferedReader(new FileReader("microcode/microcode.txt"))) {
+        printFilename(out, file);
+        try (final BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line = null;
             while ((line = br.readLine()) != null) {
-                processLine(out, line.trim());                
+                processLine(out, line.trim(), ignoreSwaps);                
             }
         }
         printFooter(out);
@@ -27,10 +29,17 @@ public class ConvertMicrocodeToHtml {
         out.println("<pre class=\"code\">");
     }
     
-    private void processLine(final PrintStream out, final String line) {
-        out.print("<span class=\"line\">");
-        final Matcher matcher = CODE_PATTERN.matcher(line);
+    private void printFilename(final PrintStream out, final File file) {
+        out.format("<span class=\"filename\">%s</span>%n", file.getName());
+    }
+    
+    private void processLine(final PrintStream out, final String line, final boolean ignoreSwaps) {        
+        final Matcher matcher = CODE_PATTERN.matcher(line);                
         if (matcher.find()) {
+            if (ignoreSwaps && matcher.group(1).startsWith("SWAP")) {
+                return;
+            }
+            out.print("<span class=\"line\">");
             out.print("<span class=\"normal\">");
             out.print(matcher.group(1));
             out.print("</span>");
@@ -43,6 +52,7 @@ public class ConvertMicrocodeToHtml {
                 out.print("</span>");
             }
         } else if (line.startsWith(";")) {
+            out.print("<span class=\"line\">");
             out.print("<span class=\"comment\">");
             out.print(line);
             out.print("</span>");
@@ -55,6 +65,11 @@ public class ConvertMicrocodeToHtml {
     }
     
     public static void main(final String... args) throws Exception {
-        new ConvertMicrocodeToHtml().launch();
+        if (args.length != 2) {
+            System.out.println("args: [ MetaTetrisScript filename ] [ ignore SWAPs (true/false) ]");
+            return;
+        }        
+        
+        new ConvertMetaTetrisScriptToHtml().launch(new File(args[0]), Boolean.parseBoolean(args[1]));
     }
 }
